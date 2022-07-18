a825c4a50a1fcf5ae893cddecce07701b37c3822
Holger H <holgheiss@gmail.com>
2022-07-18 12:21:36 +0200

    demonstrate svelte-textfit not working in svelte kit

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { textfit } from 'svelte-textfit';
+  let parent;
+</script>
+<div bind:this={parent}>
+  <h1 use:textfit={{parent,mode:"single"}}>
+    Fat headline!
+  </h1>
+</div>
